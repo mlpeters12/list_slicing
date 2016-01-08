5da602f191f40590a331b62ad31756e5288a39ae
Hackbright Student <info@hackbrightacademy.com>
2016-01-07 17:08:55 -0800

    fixed function 10

--- list_operations.py
+++ list_operations.py
@@ -170,7 +170,7 @@ def replace_head(input_list):
     """
 
     input_list[0] = 42
-    
+
 
 
 def replace_third_and_last(input_list):
@@ -187,7 +187,8 @@ def replace_third_and_last(input_list):
 
     """
 
-    pass
+    input_list[2]=37
+    input_list[-1]=37
 
 
 def replace_middle(input_list):
